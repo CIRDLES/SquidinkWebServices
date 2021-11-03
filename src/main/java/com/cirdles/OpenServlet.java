45b345a7c2d30652f3b527f12d740a760d908a83
RichardMcCarty1 <58711057+RichardMcCarty1@users.noreply.github.com>
2021-11-03 11:07:14 -0400

    Update POM.xml and add various try/catch for updated Squid3

--- src/main/java/com/cirdles/OpenServlet.java
+++ src/main/java/com/cirdles/OpenServlet.java
@@ -11,7 +11,6 @@ import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.bind.JAXBException;
 import org.cirdles.squid.Squid;
 import org.cirdles.squid.Squid3API;
 import org.cirdles.squid.Squid3Ink;
